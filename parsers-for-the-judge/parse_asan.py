3bf228cda0fe8f55423c4b5a043df8980163662f
lev872 <luisev872@gmail.com>
2024-05-19 17:10:26 +0200

    fix: tidy parser error returning tuple instead of str and asan parser error when no input is given

--- parsers-for-the-judge/parse_asan.py
+++ parsers-for-the-judge/parse_asan.py
@@ -1,18 +1,21 @@
-with open('asan.log', ) as input_file:
+with open('asan.log') as input_file:
     lines = input_file.readlines()
+    asan_output_found = False
     error_line_index = 0
     for i, line in enumerate(lines):
         if line.startswith('================================================================='):
             error_line_index = i + 1
-    index = 31
-    error_line = lines[error_line_index].split(':')
-    if error_line[1] == ' AddressSanitizer':
-        index = 34
-    error = error_line[2]
-    index = error.find(' on')
-    error = error[1:index]
+            asan_output_found = True
 
-with open('teammessage.txt', 'a') as output_file:
-    output_file.write('Error detected by Asan: \n')
-    output_file.write('ErrorId: ' + error + '\n\n')
+    if asan_output_found:
+        error_line = lines[error_line_index].split(':')
+        index = 31
+        if error_line[1] == ' AddressSanitizer':
+            index = 34
+        error = error_line[2]
+        index = error.find(' on')
+        error = error[1:index]
+
+        with open('teammessage.txt', 'a') as output_file:
+            output_file.write('ErrorId: ' + error + '\n\n')
 
--- parsers-for-the-judge/parse_yaml.py
+++ parsers-for-the-judge/parse_yaml.py
@@ -2,10 +2,6 @@ import sys
 import yaml
 
 
-def fun(x, y):
-    pass
-
-
 switcher = {'performance-avoid-endl': lambda x, y: avoidendl(x, y),
             'bugprone-narrowing-conversions': lambda x, y: narrow(x, y),
             'performance-for-range-copy': lambda x, y: rangecopy(x, y),
@@ -23,14 +19,15 @@ def avoidendl(num, error_tidy):
         return'Instead of endl you can use "\\n"'
     else:
         return 'Instead of endl you should use "\\n", endl can cause errors as it flushes the output buffer, ' \
-              'it is also less performant, as it makes more unnecessary input operations'
+               'it is also less performant, as it makes more unnecessary input operations '
 
 
 def narrow(num, error_tidy):
     if num == 0:
         return'Try avoiding operating between different types as it can cause unexpected behavior'
     else:
-        return error_tidy['DiagnosticMessage']['Message'], 'this is best avoided as it can lead to unexpected behavior'
+        return error_tidy['DiagnosticMessage']['Message'] + ', this is best avoided as it can lead to unexpected ' \
+                                                            'behavior '
 
 
 def rangecopy(num, error_tidy):
@@ -50,7 +47,7 @@ def valueparam(num, error_tidy):
     elif num == 1:
         return'You should pass arguments of expensive to copy data types by reference instead of copying them'
     else:
-        returnerror_tidy['DiagnosticMessage']['Message']
+        return error_tidy['DiagnosticMessage']['Message']
 
 
 def branchclone(num, error_tidy):
@@ -59,7 +56,7 @@ def branchclone(num, error_tidy):
     elif num == 1:
         return'Be careful with your conditional statements as you might have repeated some branches'
     else:
-        return'Be careful with your conditional statements as you might have repeated some branches' \
+        return'Be careful with your conditional statements as you might have repeated some branches, ' \
               'check if/else and switch statements that you might have copy-pasted'
 
 
@@ -69,8 +66,8 @@ def intdiv(num, error_tidy):
     elif num == 1:
         return'The result of integer division is possibly truncated before conversion'
     else:
-        return'Check your integer divisions as you might be truncating the result before' \
-              'converting to double/float. For example: double d = 3/2 resolves to 1 and then' \
+        return'Check your integer divisions as you might be truncating the result before ' \
+              'converting to double/float. For example: double d = 3/2 resolves to 1 and then ' \
               'it is casted to double, instead of the expected 1.5 you get 1.0'
 
 
@@ -89,12 +86,11 @@ def redund(num, error_tidy):
     if num == 0:
         return'There are redundant conditions in your code'
     else:
-        return'There are redundant conditions in your code, check your conditional statements as you might have' \
+        return'There are redundant conditions in your code, check your conditional statements as you might have ' \
               'copy-pasted something or review the logic for the boolean operators you have used'
 
 
 if __name__ == '__main__':
-    errorsNotFound = True
     tries = int(sys.argv[1])
     with open('clangtidy.yaml', ) as input_file:
         yaml_input = yaml.safe_load(input_file)
@@ -103,9 +99,6 @@ if __name__ == '__main__':
         for error in yaml_input['Diagnostics']:
             function = switcher.get(error['DiagnosticName'])
             if function is not None:
-                if errorsNotFound:
-                    output_file.write('Errors detected by Clang-tidy: \n')
-                    errorsNotFound = False
                 message = function(tries, error)
                 output_file.write('Error ID: ' + error['DiagnosticName'] + '\n')
                 output_file.write('Message: ' + message + '\n')
